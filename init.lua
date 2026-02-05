cdbb6adced57eded6757e4b8d0c1d0df616ef809
Juan Villalobos <juan.villalobos1992@gmail.com>
2026-02-05 10:51:02 -0800

    add code action keybinding for taking action on lint errors

--- init.lua
+++ init.lua
@@ -120,6 +120,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>ca', function()
+  vim.lsp.buf.code_action { apply = true }
+end, { desc = 'LSP Code Action (Auto-apply if unique)' })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
